af97dbf68a7f513b531f54282db1c2e4998f1eb3
kerk jazul <kerkjazul@kerks-MacBook-Air.local>
2026-01-14 12:09:40 +0800

    initial commit

---
+++ apps/desktop-ui/src/app/app.tsx
@@ -0,0 +1,13 @@
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@costmate/desktop-ui"/>
+    </div>
+  );
+}
+
+export default App;
+
+
